709ca55854bce1145b9cdac9a616a56117bf3080
fazniahd7 <108814897+fazniahd7@users.noreply.github.com>
2022-07-13 20:07:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -174,7 +174,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f" 🔥[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f" [{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -186,11 +186,9 @@ async def next_page(bot, query):
                     text=f" {file.file_name}",
                     callback_data=f'files#{file.file_id}',
                 ),
-         btn = [
-            [
 
                 InlineKeyboardButton(
-                    text=f" 🔥{get_size(file.file_size)}",
+                    text=f" {get_size(file.file_size)}",
                     callback_data=f'files_#{file.file_id}',
                 ),
             ]
@@ -1335,7 +1333,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f" 🔥[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f" [{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -1347,11 +1345,9 @@ async def auto_filter(client, msg, spoll=False):
                     text=f" {file.file_name}",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
-        btn = [
-            [
 
                 InlineKeyboardButton(
-                    text=f" 🔥{get_size(file.file_size)}",
+                    text=f" {get_size(file.file_size)}",
                     callback_data=f'{pre}_#{file.file_id}',
                 ),
             ]
