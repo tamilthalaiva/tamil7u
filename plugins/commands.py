e597d934a0c9bc2f41ad28d01eb08e41f901c95f
fazniahd7 <108814897+fazniahd7@users.noreply.github.com>
2022-07-13 11:13:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -239,7 +239,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='youtube.com/OpusTechz') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🇯‌🇴‌🇮‌🇳‌', url='https://t.me/TamilRockers_FZN') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
