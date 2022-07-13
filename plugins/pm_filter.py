68fb7264568faa62a165f193d5ff9948b2512b47
fazniahd7 <108814897+fazniahd7@users.noreply.github.com>
2022-07-13 19:52:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -174,7 +174,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f" {file.file_name} 🔥[{get_size(file.file_size)}]", callback_data=f'files#{file.file_id}'
+                    text=f" 🔥[{get_size(file.file_size)}]  {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -1332,7 +1332,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f" {file.file_name} 🔥[{get_size(file.file_size)}]", callback_data=f'{pre}#{file.file_id}'
+                    text=f" 🔥[{get_size(file.file_size)}]  {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
