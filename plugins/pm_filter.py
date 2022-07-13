8a66128cf4b33a3c6e289b801a8c292d0389f56d
fazniahd7 <108814897+fazniahd7@users.noreply.github.com>
2022-07-13 16:08:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -183,7 +183,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⇏ {file.file_name} 🔥[{get_size(file.file_size)}]", callback_data=f'files#{file.file_id}'
+                    text=f"➡ {file.file_name}🔥[{get_size(file.file_size)}]", callback_data=f'files#{file.file_id}'
                 ),
                 InlineKeyboardButton(
                     text=f"🔥{get_size(file.file_size)}",
@@ -1331,7 +1331,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⇏ {file.file_name} 🔥[{get_size(file.file_size)}] ", callback_data=f'{pre}#{file.file_id}'
+                    text=f"➡ {file.file_name}🔥[{get_size(file.file_size)}] ", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -1340,7 +1340,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⇏ {file.file_name}",
+                    text=f"➡ {file.file_name}",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
                 InlineKeyboardButton(
