8a8e7a48cafcf4a0aae196f0f02d0cfc8476d269
fazniahd7 <108814897+fazniahd7@users.noreply.github.com>
2022-07-13 17:20:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -174,7 +174,8 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file_name} [{get_size(file.file_size)}]", callback_data=f'files#{file.file_id}'
+                    text=f"{file.file_name}
+                    [{get_size(file.file_size)}]", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -187,7 +188,7 @@ async def next_page(bot, query):
                     callback_data=f'files_#{file.file_id}',
                 ),
                 InlineKeyboardButton(
-                    text=f" {file_name}",
+                    text=f" {file.file_name}",
                     callback_data=f'files#{file.file_id}',
                 ),
             ]
@@ -1332,7 +1333,8 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file_name} [{get_size(file.file_size)}]", callback_data=f'{pre}#{file.file_id}'
+                    text=f"{file.file_name}
+                    [{get_size(file.file_size)}]", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -1345,7 +1347,7 @@ async def auto_filter(client, msg, spoll=False):
                     callback_data=f'{pre}_#{file.file_id}',
                 ),
                 InlineKeyboardButton(
-                    text=f" {file_name}",
+                    text=f" {file.file_name}",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
             ]
