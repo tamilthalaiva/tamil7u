9a3e19231c0de4cff61984f6345186e80248374a
fazniahd7 <108814897+fazniahd7@users.noreply.github.com>
2022-07-13 17:10:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -174,7 +174,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"➜ {file.file_name}🔥[{get_size(file.file_size)}]", callback_data=f'files#{file.file_id}'
+                    text=f"{file.file_name} [{get_size(file.file_size)}]", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -183,11 +183,11 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🔥 {get_size(file.file_size)}",
+                    text=f" {get_size(file.file_size)}",
                     callback_data=f'files_#{file.file_id}',
                 ),
                 InlineKeyboardButton(
-                    text=f"➜ {file.file_name}",
+                    text=f" {file.file_name}",
                     callback_data=f'files#{file.file_id}',
                 ),
             ]
@@ -1332,7 +1332,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"➜ {file.file_name}🔥[{get_size(file.file_size)}]", callback_data=f'{pre}#{file.file_id}'
+                    text=f"{file.file_name} [{get_size(file.file_size)}]", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -1341,11 +1341,11 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🔥 {get_size(file.file_size)}",
+                    text=f" {get_size(file.file_size)}",
                     callback_data=f'{pre}_#{file.file_id}',
                 ),
                 InlineKeyboardButton(
-                    text=f"➜ {file.file_name}",
+                    text=f" {file.file_name}",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
             ]
