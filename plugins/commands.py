347c6a0f48bf58516352740b563cd32de8e004ef
fazniahd7 <108814897+fazniahd7@users.noreply.github.com>
2022-07-13 10:04:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -51,7 +51,7 @@ async def start(client, message):
         ]]         
         reply_markup = InlineKeyboardMarkup(buttons)        
         await message.reply_video(
-            Video=random.choice(PICS),
+            video=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
