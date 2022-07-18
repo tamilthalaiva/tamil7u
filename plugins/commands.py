3f5dd2ae03f8ec30728f72b7ea6395a510b865f4
ankitpatel005 <108638996+ankitpatel005@users.noreply.github.com>
2022-07-18 15:08:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -239,7 +239,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🇯‌🇴‌🇮‌🇳‌', url='https://t.me/TamilRockers_FZN') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🇯‌🇴‌🇮‌🇳‌', url='https://t.me/+UAPWlbyITcdmOTZl') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
