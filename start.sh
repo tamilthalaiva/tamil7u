b4cde7d2bef46555382d7b06a05eb14303809121
ankitpatel005 <108638996+ankitpatel005@users.noreply.github.com>
2022-08-10 04:26:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/ankitpatel005/auto-filter-bot-repo
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Ankit
 fi
-cd /AJAX
+cd /APZ
 pip3 install -U -r requirements.txt
 echo "Starting APZ....🔥"
 python3 bot.py
