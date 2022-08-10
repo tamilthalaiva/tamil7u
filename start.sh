1ec43a87c0e165c5ba9ac033f2bf9e049e2e55b9
ankitpatel005 <108638996+ankitpatel005@users.noreply.github.com>
2022-08-10 09:11:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/ankitpatel005/auto-filter-bot-repo
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ankit
+  git clone $UPSTREAM_REPO /auto-filter-bot-repo
 fi
-cd /Ajax
+cd /auto-filter-bot-repo
 pip3 install -U -r requirements.txt
 echo "Starting APZ....🔥"
 python3 bot.py
