9b0b3ad0fec7076a7f0d50765f8c2b67085a913d
ankitpatel005 <108638996+ankitpatel005@users.noreply.github.com>
2022-08-10 20:38:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ankitpatel005/auto-filter-bot-repo.git /auto-filter-bot-repo
+  git clone https://github.com/ankitpatel005/APZ-EXTRA-FEATURES.git /APZ-EXTRA-FEATURES
+else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /auto-filter-bot-repo
-cd /auto-filter-bot-repo
+  git clone $UPSTREAM_REPO /APZ-EXTRA-FEATURES
+fi
+cd /APZ-EXTRA-FEATURES
 pip3 install -U -r requirements.txt
 echo "Starting Bot....💥"
 python3 bot.py
