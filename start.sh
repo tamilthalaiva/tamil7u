f5c1287dacb46bdab1ea5847ea9901bf6fbbecc7
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-08-11 18:33:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/ankitpatel005/APZ-EXTRA-FEATURES.git /APZ-EXTRA-FEATURES
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /APZ-EXTRA-FEATURES
-fi
+echo "Cloning Repo, Please Wait..."
+git clone -b main https://github.com/ankitpatel005/APZ-EXTRA-FEATURES.git /APZ-EXTRA-FEATURES
 cd /APZ-EXTRA-FEATURES
+echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting Bot....💥"
+echo "Starting Bot, Please Wait..."
 python3 bot.py
